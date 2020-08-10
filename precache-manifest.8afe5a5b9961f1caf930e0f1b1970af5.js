ef42d3bd1caf3c5536297e9a71a7f45e61723c9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-10 10:39:34 +0000

    Deploy zhangwudi886/yanfadashi-lib to github.com/zhangwudi886/yanfadashi-lib.git:gh-pages

---
+++ precache-manifest.8afe5a5b9961f1caf930e0f1b1970af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51487aae6feb84441dc2e57ad9b03c31",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.dcf751fd6db10b641c23.bundle.js"
+  },
+  {
+    "url": "runtime~main.dcf751fd6db10b641c23.bundle.js"
+  },
+  {
+    "revision": "77315dfc04be62fd7c2d",
+    "url": "static/css/main.c5b901d6.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.dcf751fd6db10b641c23.bundle.js"
+  },
+  {
+    "url": "vendors~main.dcf751fd6db10b641c23.bundle.js.LICENSE.txt"
+  }
+]);
